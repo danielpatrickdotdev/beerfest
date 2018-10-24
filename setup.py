9725ef4c4b4bc72cfa8ccc48e9a8518e492be0b8
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-24 16:28:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="beerfest",
+    version="0.0.1a1",
+    author="Daniel Patrick",
+    author_email="danieljudepatrick@gmail.com",
+    description="Django app for beer festival data",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/remarkablerocket/beerfest",
+    packages=[
+        "beerfest",
+    ],
+    install_requires=[
+        "django>=2.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Framework :: Django :: 2.1",
+        "Inteded Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
