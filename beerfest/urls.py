5bd3d9a0ec6a9298467204957169dfff941b221d
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-25 14:58:22 +0100

    Flake8

--- beerfest/urls.py
+++ beerfest/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import include
 from django.urls import path
 
 import beerfest.views
@@ -7,6 +6,8 @@ urlpatterns = [
     path('', beerfest.views.index, name='index'),
     path('beers/', beerfest.views.beer_list, name='beer_list'),
     path('beers/<int:id>', beerfest.views.beer_detail, name='beer_detail'),
-    path('beers/star_beer/<int:id>', beerfest.views.star_beer, name='star_beer'),
-    path('beers/unstar_beer/<int:id>', beerfest.views.unstar_beer, name='unstar_beer'),
+    path('beers/star_beer/<int:id>',
+         beerfest.views.star_beer, name='star_beer'),
+    path('beers/unstar_beer/<int:id>',
+         beerfest.views.unstar_beer, name='unstar_beer'),
 ]
