0e3996351c1d7c1eadeaabdd69591fb9b66c7213
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-26 09:12:45 +0100

    Test admin registrations

---
+++ tests/test_admin.py
@@ -0,0 +1,21 @@
+from django.contrib.admin import site as admin_site
+from django.test import TestCase
+
+from beerfest.models import Bar, Brewery, Beer, UserBeer
+
+
+class TestBeerfestAdmin(TestCase):
+    def setUp(self):
+        self.registry = admin_site._registry
+
+    def test_bar_registered_with_admin(self):
+        self.assertIn(Bar, self.registry)
+
+    def test_brewery_registered_with_admin(self):
+        self.assertIn(Brewery, self.registry)
+
+    def test_beer_registered_with_admin(self):
+        self.assertIn(Beer, self.registry)
+
+    def test_userbeer_registered_with_admin(self):
+        self.assertIn(UserBeer, self.registry)
