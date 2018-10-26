3ac6df08bb555155fd7f62c8c75ab7d60567b4b7
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-26 11:24:31 +0100

    Improve URL scheme consistency

--- beerfest/urls.py
+++ beerfest/urls.py
@@ -5,9 +5,9 @@ import beerfest.views
 urlpatterns = [
     path('', beerfest.views.index, name='index'),
     path('beers/', beerfest.views.beer_list, name='beer_list'),
-    path('beers/<int:id>', beerfest.views.beer_detail, name='beer_detail'),
-    path('beers/star_beer/<int:id>',
+    path('beers/<int:id>/', beerfest.views.beer_detail, name='beer_detail'),
+    path('beers/<int:id>/star/',
          beerfest.views.star_beer, name='star_beer'),
-    path('beers/unstar_beer/<int:id>',
+    path('beers/<int:id>/unstar/',
          beerfest.views.unstar_beer, name='unstar_beer'),
 ]
