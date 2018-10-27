ba16d7009f961f0f1cc124d43e13012339f99386
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-27 19:19:08 +0100

    Reorganise URL tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -5,7 +5,7 @@ from django.urls import resolve, reverse
 from beerfest import models, views
 
 
-class TestURLs(TestCase):
+class URLTestBase(TestCase):
     def setUp(self):
         bar = models.Bar.objects.create(name="Test Bar")
         brewery = models.Brewery.objects.create(
@@ -15,30 +15,40 @@ class TestURLs(TestCase):
         user = User.objects.create(username="Mx Test")
         models.UserBeer.objects.create(user=user, beer=beer)
 
+
+class TestIndexURL(URLTestBase):
     def test_index_route_uses_index_view(self):
         self.assertEqual(resolve("/").func, views.index)
 
     def test_index_route_reverse(self):
         self.assertEqual(reverse("index"), "/")
 
+
+class TestBeerListURL(URLTestBase):
     def test_beer_list_route_uses_beer_list_view(self):
         self.assertEqual(resolve("/beers/").func, views.beer_list)
 
     def test_beer_list_route_reverse(self):
         self.assertEqual(reverse("beer_list"), "/beers/")
 
+
+class TestBeerDetailURL(URLTestBase):
     def test_beer_detail_route_uses_beer_detail_view(self):
         self.assertEqual(resolve("/beers/1/").func, views.beer_detail)
 
     def test_beer_detail_route_reverse(self):
         self.assertEqual(reverse("beer_detail", args=(1,)), "/beers/1/")
 
+
+class TestStarBeerURL(URLTestBase):
     def test_star_beer_route_uses_star_beer_view(self):
         self.assertEqual(resolve("/beers/1/star/").func, views.star_beer)
 
     def test_star_beer_route_reverse(self):
         self.assertEqual(reverse("star_beer", args=(1,)), "/beers/1/star/")
 
+
+class TestUnstarBeerURL(URLTestBase):
     def test_unstar_beer_route_uses_unstar_beer_view(self):
         self.assertEqual(resolve("/beers/1/unstar/").func, views.unstar_beer)
 
