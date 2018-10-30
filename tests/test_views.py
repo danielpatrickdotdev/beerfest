244dbef228156cf0b85aa98149630a02ca587ca3
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-30 15:20:10 +0000

    Create first view test

---
+++ tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+
+from beerfest import views
+
+
+class BaseViewTest(TestCase):
+    def setUp(self):
+        self.login_url = "/accounts/login/"
+        self.factory = RequestFactory()
+
+
+class TestIndexView(BaseViewTest):
+    def test_redirects_to_beer_list(self):
+        request = self.factory.get("")
+        response = views.index(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["Location"], reverse("beer-list"))
