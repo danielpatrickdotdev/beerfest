36ff221a5117a8568ab17038e57532dc36d890ee
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-31 15:08:07 +0000

    Test user_profile view

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
@@ -18,3 +19,36 @@ class TestIndexView(BaseViewTest):
         response = views.IndexView.as_view()(request)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response["Location"], reverse("beer-list"))
+
+
+class TestUserProfileView(BaseViewTest):
+    def test_context_variables_and_template_used(self):
+        bar = factories.create_bar()
+        brewery = factories.create_brewery()
+        beer1 = factories.create_beer(bar=bar, brewery=brewery, name="Star IPA")
+        beer2 = factories.create_beer(bar=bar, brewery=brewery, name="Try PA")
+        beer3 = factories.create_beer(bar=bar, brewery=brewery, name="Best")
+        factories.create_beer(bar=bar, brewery=brewery, name="Unpopular Swill")
+
+        factories.create_user_beer(user=self.user, beer=beer1, starred=True)
+        factories.create_user_beer(user=self.user, beer=beer2, starred=False,
+                                   tried=True)
+        factories.create_user_beer(user=self.user, beer=beer3, starred=False,
+                                   rating=5)
+
+        self.client.force_login(self.user)
+        response = self.client.get("/accounts/profile/")
+
+        beer_list = list(response.context["beer_list"])
+        self.assertCountEqual(beer_list, [beer1, beer2, beer3])
+        user = response.context["user"]
+        self.assertEqual(user, self.user)
+        self.assertTemplateUsed(response, "beerfest/user_profile.html")
+
+    def test_unauthenticated_user_gets_redirected_to_login(self):
+        request = self.factory.get("")
+        request.user = AnonymousUser()
+        response = views.user_profile(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["Location"], self.login_url + "?next=/")
