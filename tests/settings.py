1552ec4e07a779c0d67344acde9745a069cfc068
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-31 15:03:38 +0000

    Update test settings
    Allow django to find templates
    Install app and middlewares required for some tests

--- tests/settings.py
+++ tests/settings.py
@@ -19,10 +19,28 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
     "django.contrib.sites",
     "beerfest",
 ]
 
+MIDDLEWARE = (
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+)
+
 SITE_ID = 1
 
-MIDDLEWARE = ()
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["tests/templates"],
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+            ],
+        },
+    },
+]
